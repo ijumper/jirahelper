e3db58fe12a3166c2a721b4929f70f9ca393b827
brianmason <brian.mason@kingfisher.com>
2016-08-07 22:11:14 +0100

    Added Spring boot application

---
+++ jirahelper-client/src/main/java/uk/co/ijump/jirahelper/client/MainApplication.java
@@ -0,0 +1,19 @@
+package uk.co.ijump.jirahelper.client;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+
+
+    }
+
+}
